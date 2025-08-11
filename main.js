2d161c5f92042d481db89cc73ebb6044a114b784
mohammedAljadd <aljadd3@gmail.com>
2025-08-11 09:22:02 +0200

    add controls

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // Renderer
 const canvas = document.querySelector('#c');
@@ -9,6 +9,10 @@ const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 // Better resolution
 renderer.setSize( window.innerWidth, window.innerHeight );
 
+
+
+
+
 // Setup a camera
 const fov = 75;
 const aspect = window.innerWidth / window.innerHeight;
@@ -16,10 +20,16 @@ const near = 0.1;
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
+
+// Controls
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
 // Seeing the house from a good angle
 camera.position.set(-15, -15, 20);
 camera.lookAt(0, 0, 0);
 
+controls.update(); //controls.update() must be called after any manual changes to the camera's transform
 
 // Our scene
 const scene = new THREE.Scene();
